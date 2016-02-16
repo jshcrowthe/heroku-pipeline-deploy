2ba679570ab01e8aeb8623475f19cfb57f44ed06
Josh Crowther <jshcrowthe@gmail.com>
2016-02-15 23:06:54 -0700

    Remove unneeded code. Update docs

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
 heroku-pipeline-deploy
 --------------------------------
 
-This step allows you to deploy a wercker build to a heroku pipeline.
+This step allows you to promote a wercker deploy within a heroku pipeline. It is designed to work with a [heroku deploy step](https://app.wercker.com/#applications/51c829e73179be4478002157/tab/details) and not as a standalone deployment step.
+
+## Options
+There are 4 options (3 required) that can be passed in your `wercker.yml`:
+
+- `user` This is the username that will be used to interact with the heroku toolbelt.
+- `key` This is the API key that corresponds to the `user` property.
+- `from` The name of the heroku app that you will be promoting.
+- `to` _(optional)_ The destination app you will be promoting to. This can be omitted if you want to promote to the default promotion targets.
+
+## Example Usage
+As mentioned before, this step is **not** a standalone step. It should be used in addition to a [heroku deploy step](https://app.wercker.com/#applications/51c829e73179be4478002157/tab/details) for best experience. In the following example a deploy is triggered to a reference (your dev environment) and then it is promoted to the next environment (likely a beta or other pre-prod reference).
+
+```yaml
+deploy:
+  steps:
+    - heroku-deploy:
+        key: API_KEY
+        user: API_USERNAME
+        app-name: SOURCE_APP_NAME
+    - heroku-pipeline-deploy:
+        key: API_KEY
+        user: API_USERNAME
+        from: SOURCE_APP_NAME
+```
+You can chain multiple `heroku-pipeline-deploy` steps together to promote as many times as you'd like
+
+### Sources
+- https://github.com/wercker/step-heroku-deploy (Heroku Deploy Step)
+- https://devcenter.heroku.com/articles/pipelines (Heroku Pipeline Documentation)
--- run.sh
+++ run.sh
@@ -2,44 +2,29 @@
 set -e;
 
 main() {
-  local exit_code_push=0;
+  local exit_code_promote=0;
   local exit_code_run=0;
-  local gitssh_path;
-  local ssh_key_path;
-
-  gitssh_path="$(mktemp)";
-  ssh_key_path="$(mktemp -d)/id_rsa";
 
   # Initialize some values
   init_wercker_environment_variables;
   init_netrc "$WERCKER_HEROKU_PIPELINE_DEPLOY_USER" "$WERCKER_HEROKU_PIPELINE_DEPLOY_KEY";
-  init_ssh;
-  init_git "$WERCKER_HEROKU_PIPELINE_DEPLOY_USER" "$WERCKER_HEROKU_PIPELINE_DEPLOY_USER";
-  init_gitssh "$gitssh_path" "$ssh_key_path";
 
   # Test authentication to verify it works
   test_authentication "$WERCKER_HEROKU_PIPELINE_DEPLOY_APP_NAME";
 
-  # Check if the user supplied a wercker key-name
-  if [ -n "$WERCKER_HEROKU_PIPELINE_DEPLOY_KEY_NAME" ]; then
-    use_wercker_ssh_key "$ssh_key_path" "$WERCKER_HEROKU_PIPELINE_DEPLOY_KEY_NAME";
-  else
-    use_random_ssh_key "$ssh_key_path";
-  fi
-
   # Install heroku toolbelt to do promotions
   install_toolbelt;
 
-  # Promote from -int to -beta
+  # Promote application
   if [[ -z $WERCKER_HEROKU_PIPELINE_DEPLOY_TO ]]; then
-    heroku pipelines:promote -a "$WERCKER_HEROKU_PIPELINE_DEPLOY_FROM"
+    heroku pipelines:promote -a "$WERCKER_HEROKU_PIPELINE_DEPLOY_FROM" || exit_code_promote=1
   else
-    heroku pipelines:promote -a "$WERCKER_HEROKU_PIPELINE_DEPLOY_FROM" --to "$WERCKER_HEROKU_PIPELINE_DEPLOY_TO"
+    heroku pipelines:promote -a "$WERCKER_HEROKU_PIPELINE_DEPLOY_FROM" --to "$WERCKER_HEROKU_PIPELINE_DEPLOY_TO" || exit_code_promote=1
   fi
 
   # Run a command, if the push succeeded and the user supplied a run command
   if [ -n "$WERCKER_HEROKU_PIPELINE_DEPLOY_RUN" ]; then
-    if [ $exit_code_push -eq 0 ]; then
+    if [ $exit_code_promote -eq 0 ]; then
       set +e;
       execute_heroku_command "$WERCKER_HEROKU_PIPELINE_DEPLOY_APP_NAME" "$WERCKER_HEROKU_PIPELINE_DEPLOY_RUN";
       exit_code_run=$?
@@ -47,19 +32,14 @@ main() {
     fi
   fi
 
-  # Remove a auto generated key (assuming we generated a public key at ${ssh_key_path}.pub)
-  if [ -z "$WERCKER_HEROKU_PIPELINE_DEPLOY_KEY_NAME" ]; then
-    remove_ssh_key "${ssh_key_path}.pub";
-  fi
-
   if [ $exit_code_run -ne 0 ]; then
-    fail 'heroku run failed';
+    fail "Heroku run failed (Command: $WERCKER_HEROKU_PIPELINE_DEPLOY_RUN)";
   fi
 
-  if [ $exit_code_push -eq 0 ]; then
-    success 'deployment to heroku finished successfully';
+  if [ $exit_code_promote -eq 0 ]; then
+    success 'Heroku app promotion finished successfully';
   else
-    fail 'git push to heroku failed';
+    fail "Heroku promote for app $WERCKER_HEROKU_PIPELINE_DEPLOY_FROM failed";
   fi
 }
 
@@ -78,22 +58,6 @@ init_wercker_environment_variables() {
   if [ -z "$WERCKER_HEROKU_PIPELINE_DEPLOY_FROM" ]; then
     fail "from property is required"
   fi
-
-  if [ -z "$WERCKER_HEROKU_PIPELINE_DEPLOY_USER" ]; then
-    if [ ! -z "$HEROKU_USER" ]; then
-      export WERCKER_HEROKU_PIPELINE_DEPLOY_USER="$HEROKU_USER";
-    else
-      export WERCKER_HEROKU_PIPELINE_DEPLOY_USER="heroku-deploy@wercker.com";
-    fi
-  fi
-
-  if [ -z "$WERCKER_HEROKU_PIPELINE_DEPLOY_SOURCE_DIR" ]; then
-    export WERCKER_HEROKU_PIPELINE_DEPLOY_SOURCE_DIR="$WERCKER_ROOT";
-    debug "option source_dir not set. Will deploy directory $WERCKER_HEROKU_PIPELINE_DEPLOY_SOURCE_DIR";
-  else
-    warn "Use of source_dir is deprecated. Please make sure that you fix your Heroku deploy version on a major version."
-    debug "option source_dir found. Will deploy directory $WERCKER_HEROKU_PIPELINE_DEPLOY_SOURCE_DIR";
-  fi
 }
 
 # init_netrc($username, $password) appends the machine credentials for Heroku to
@@ -110,46 +74,6 @@ init_netrc() {
   } >> "$netrc"
 
   chmod 0600 "$netrc";
-}
-
-# init_ssh will add the public key of Heroku to the known_hosts file (after
-# making sure it exists, and has proper permissions)
-init_ssh() {
-  local heroku_public_key="heroku.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAu8erSx6jh+8ztsfHwkNeFr/SZaSOcvoa8AyMpaerGIPZDB2TKNgNkMSYTLYGDK2ivsqXopo2W7dpQRBIVF80q9mNXy5tbt1WE04gbOBB26Wn2hF4bk3Tu+BNMFbvMjPbkVlC2hcFuQJdH4T2i/dtauyTpJbD/6ExHR9XYVhdhdMs0JsjP/Q5FNoWh2ff9YbZVpDQSTPvusUp4liLjPfa/i0t+2LpNCeWy8Y+V9gUlDWiyYwrfMVI0UwNCZZKHs1Unpc11/4HLitQRtvuk0Ot5qwwBxbmtvCDKZvj1aFBid71/mYdGRPYZMIxq1zgP1acePC1zfTG/lvuQ7d0Pe0kaw==";
-
-  mkdir -p "$HOME/.ssh";
-  touch "$HOME/.ssh/known_hosts";
-  chmod 600 "$HOME/.ssh/known_hosts";
-  echo "$heroku_public_key" >> "$HOME/.ssh/known_hosts";
-}
-
-# init_git checks that git exists, and that
-init_git() {
-  local username="$1";
-  local email="$2";
-
-  if ! type git &> /dev/null; then
-    if ! type apt-get &> /dev/null; then
-      fail "git is not available. Install it, and make sure it is available in \$PATH"
-    else
-      debug "git not found; installing it."
-
-      sudo apt-get update;
-      sudo apt-get install git-core -y;
-    fi
-  fi
-
-  git config --global user.name "$username";
-  git config --global user.email "$email";
-}
-
-init_gitssh() {
-  local gitssh_path="$1";
-  local ssh_key_path="$2";
-
-  echo "ssh -e none -i \"$ssh_key_path\" \$@" > "$gitssh_path";
-  chmod 0700 "$gitssh_path";
-  export GIT_SSH="$gitssh_path";
 }
 
 install_toolbelt() {
@@ -175,50 +99,6 @@ install_toolbelt() {
   heroku plugins:install heroku-pipelines
 }
 
-use_wercker_ssh_key() {
-  local ssh_key_path="$1";
-  local wercker_ssh_key_name="$2";
-
-  debug "will use specified key in key-name option: ${wercker_ssh_key_name}_PRIVATE";
-
-  local private_key;
-  private_key=$(eval echo "\$${wercker_ssh_key_name}_PRIVATE");
-
-  if [ ! -n "$private_key" ]; then
-    fail 'Missing key error. The key-name is specified, but no key with this name could be found. Make sure you generated an key, and exported it as an environment variable.';
-  fi
-
-  debug "writing key file to $ssh_key_path";
-  echo -e "$private_key" > "$ssh_key_path";
-  chmod 0600 "$ssh_key_path";
-}
-
-use_random_ssh_key() {
-  local ssh_key_path="$1";
-
-  local ssh_key_comment="deploy-$RANDOM@wercker.com";
-
-  debug "no key-name specified, will generate key and add it to heroku";
-
-  debug 'generating random ssh key for this deploy';
-  ssh-keygen -f "$ssh_key_path" -C "$ssh_key_comment" -N '' -t rsa -q -b 4096;
-  debug "generated ssh key $ssh_key_comment for this deployment";
-  chmod 0600 "$ssh_key_path";
-
-  add_ssh_key "${ssh_key_path}.pub";
-}
-
-push_code() {
-  local app_name="$1";
-
-  debug "starting heroku deployment with git push";
-  git push -f "git@heroku.com:$app_name.git" HEAD:master;
-  local exit_code_push=$?;
-
-  debug "git pushed exited with $exit_code_push";
-  return $exit_code_push;
-}
-
 execute_heroku_command() {
   local app_name="$1";
   local command="$2";
@@ -229,38 +109,6 @@ execute_heroku_command() {
 
   debug "heroku run exited with $exit_code_run";
   return $exit_code_run;
-}
-
-add_ssh_key() {
-  local public_key_path="$1";
-
-  local public_key;
-  public_key=$(cat "$public_key_path");
-
-  debug "Adding ssh key to Heroku account"
-
-  curl -n -X POST https://api.heroku.com/account/keys \
-  -H "Accept: application/vnd.heroku+json; version=3" \
-  -H "Content-Type: application/json" \
-  -d "{\"public_key\":\"$public_key\"}" > /dev/null 2>&1;
-}
-
-calculate_fingerprint() {
-  local public_key_path="$1";
-
-  ssh-keygen -lf "$public_key_path" | awk '{print $2}';
-}
-
-remove_ssh_key() {
-  local public_key_path="$1";
-
-  local fingerprint;
-  fingerprint=$(calculate_fingerprint "$public_key_path");
-
-  debug "Removing ssh key from Heroku account (fingerprint: $fingerprint)"
-
-  curl -n -X DELETE "https://api.heroku.com/account/keys/$fingerprint" \
-  -H "Accept: application/vnd.heroku+json; version=3" > /dev/null 2>&1;
 }
 
 test_authentication() {
@@ -276,7 +124,7 @@ test_authentication() {
   set -e;
 
   if [ $exit_code_authentication_test -ne 0 ]; then
-    fail 'Unable to retrieve account information, please update your Heroku API key';
+    fail 'Unable to retrieve account information, please check your Heroku Username and API key';
   fi
 
   set +e;
